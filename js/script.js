3d7b0412104ffda8b40349a46f5246cd67b1b1f9
Raphael Angelo Floresca <rpngfl@gmail.com>
2021-03-28 11:12:46 +0800

    Attempted to solve page animation issue, will come back when JS knowledge more advanced

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 // DOM assigns a variable to an HTML class
 const menuButton = document.querySelector('.toggle');
 const navigation = document.querySelector('.navigation');
-const navigationLinks = document.querySelectorAll('.navigation ul li a')
+const navLinks = document.querySelectorAll('.navigation ul li a')
+
+for (const navLink of navLinks) {
+    navLink.addEventListener('click', () => {
+        window.onload = () => {
+            navigation.style.animation = "navigationTransition 1s";
+        }
+    })
+}
+
 
 // When menuButton is clicked, the active class is toggled on menuButton and navigation
 menuButton.addEventListener('click', () => {
